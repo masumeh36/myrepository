7c2fad3f0d8324e95cfa9f5497bda14b214e0ffd
masumeh36 <m_g1_d@yahoo.com>
2019-07-04 11:41:43 +0430

    Updates

---
+++ precache-manifest.968938b2faf96cd060995bcd8697e545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0e87899da7775949b6a65db934395f",
+    "url": "/myrepository/index.html"
+  },
+  {
+    "revision": "5c370aa66f881dbd8936",
+    "url": "/myrepository/static/css/main.f07fc0dc.chunk.css"
+  },
+  {
+    "revision": "aeec392463af2b1ccd65",
+    "url": "/myrepository/static/js/2.64f36e7b.chunk.js"
+  },
+  {
+    "revision": "5c370aa66f881dbd8936",
+    "url": "/myrepository/static/js/main.31b60761.chunk.js"
+  },
+  {
+    "revision": "c9e2f25e987f63fc14a7",
+    "url": "/myrepository/static/js/runtime~main.8e5c26c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myrepository/static/media/logo.5d5d9eef.svg"
+  }
+]);
